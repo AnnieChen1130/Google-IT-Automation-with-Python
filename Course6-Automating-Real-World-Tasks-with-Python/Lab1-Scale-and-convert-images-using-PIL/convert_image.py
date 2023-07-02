572d2b41ce0da1ad332f2dd532ef7eabcf1f895e
AnnieChen1130 <chenzhijun1130@gmail.com>
2023-07-01 22:41:10 -0500

    Add files via upload

---
+++ Course6-Automating-Real-World-Tasks-with-Python/Lab1-Scale-and-convert-images-using-PIL/convert_image.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+import os
+from PIL import Image
+
+source_dir = "images/"
+output_dir = "/opt/icons/"
+
+for filename in os.listdir(source_dir):
+    if not filename.startswith('.'):
+        im = Image.open(os.path.join(source_dir, filename))
+        im = im.rotate(90).resize((128,128)).convert("RGB")
+        im.save(os.path.join(output_dir, filename+".jpeg"))
